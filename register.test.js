03a0502bcf68f8025dce7892585af2e6d30fd454
Parth <prbharvad@myseneca.ca>
2025-07-25 17:44:35 -0400

    Update register.test.js

--- register.test.js
+++ register.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./server'); // ✅ correct
+const app = require('./server'); // 
 
 describe('POST /users/register', () => {
   it('should register a new user', async () => {
